7547427b697af4c975691f10e2c9f61f81719d17
Nelson Zaraza <nelsonmauricionavarrozaraza@gmail.com>
2025-05-19 22:16:34 -0500

    Se agrego whatsapp como nueva red social

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,15 @@
-import React from "react";
 import github from "../../assets/github_light.svg";
 import gmail from "../../assets/gmail.svg";
 import instagram from "../../assets/instagram.svg";
 import linkedin from "../../assets/linkedin.svg";
 import tiktok from "../../assets/tiktok.svg";
 import youtube from "../../assets/youtube.svg";
+import whatsapp from "../../assets/whatsapp.svg";
 
 export default function Contact() {
   return (
     <>
-      <div className="flex justify-center space-x-2 my-6">
+      <div className="flex flex-wrap justify-center space-x-2 my-6">
         {redesSociales.map((redSocial, index) => (
           <a key={index} className="bg-lightPurple rounded-full flex justify-center items-center hover:-translate-y-2 hover:duration-300 shadow-xl/30" href={redSocial.link} target="_blank">
             <img className="w-8 h-8 m-2" src={redSocial.icono} alt={redSocial.icono} />
@@ -24,6 +24,7 @@ const redesSociales = [
   { icono: github, link: "https://github.com/NelsonZarazaDev" },
   { icono: linkedin, link: "https://www.linkedin.com/in/nelson-mauricio-navarro-zaraza-3448542a5/" },
   { icono: gmail, link: "mailto:nelsonmauricionavarrozaraza@gmail.com" },
+  { icono: whatsapp, link: "https://wa.me/573157465208" },
   { icono: instagram, link: "https://www.instagram.com/nelson_mauricio_z/" },
   { icono: tiktok, link: "https://www.tiktok.com/@nelson_mauricio_z" },
   { icono: youtube, link: "https://www.youtube.com/@Ctrl_Code" },
